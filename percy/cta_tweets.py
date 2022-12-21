59d56e5456f62eaf42fe660aef539595580cce00
Nathaniel Wise <nathaniel.wise@gmail.com>
2022-12-21 09:46:53 -0500

    added map generation with marker for trains

--- percy/cta_tweets.py
+++ percy/cta_tweets.py
@@ -5,19 +5,63 @@ https://www.transitchicago.com/station/wils/
 https://docs.mapbox.com/api/maps/static-images/
 """
 
-from credentials import TRAIN_TRACKER_KEY
+from credentials import TRAIN_TRACKER_KEY, google_key
 from cta_parse import CTAStation, Prediction, getData, printArrivals, stations
 import requests
 import json
 
 st_95th = CTAStation(12)
 data_95th = getData(12)
+
+
+base_url = "https://maps.googleapis.com/maps/api/staticmap"
+zoom = 16
+size = "500x1000"
+type = "roadmap"
+counter = 0
+train_markers = {}
+
 for i in data_95th:
     print(i["rt"], end=" ")
     print(i["rn"], end=" ")
     if int(i["isApp"]):
+        pass
         print("is approaching the station.")
     elif i["lat"]:
         print(f' is at {i["lat"]}, {i["lon"]} ')
+
+        lat = float(i["lat"])
+        lon = float(i["lon"])
+        marker_label = f'{i["rn"]}'
+        marker = f"{lat},{lon}|label:{marker_label}"
+
+        query_string = f"center={lat},{lon}&zoom={zoom}&size={size}&maptype={type}&markers={marker}&key={google_key}"
+
+        response = requests.get(base_url + "?" + query_string)
+
+        #with open(f"map{0+counter}.png", "wb") as f:
+        #    f.write(response.content)
+
+        
+        train_markers[f'{counter}'] = {}
+        train_markers[f'{counter}']["lat"] = float(i["lat"])
+        train_markers[f'{counter}']["lon"] = float(i["lon"])
+        train_markers[f'{counter}']["marker_label"] = f'{i["rn"]}'
+        train_markers[f'{counter}']["marker"] = f"{lat},{lon}|label:{marker_label}"
+
+        counter += 1
     else:
         print(" is at an unknown location.")
+
+markers_query = ""
+for i in range(len(train_markers)):
+    markers_query += f'&markers={train_markers[str(i)]["marker"]}'
+
+zoom = 11
+size = "300x1500"
+query_string = f"center={41.823815},{-87.630285}&zoom={zoom}&size={size}&maptype={type}{markers_query}&key={google_key}"
+
+response = requests.get(base_url + "?" + query_string)
+
+with open(f"mapall.png", "wb") as f:
+    f.write(response.content)
